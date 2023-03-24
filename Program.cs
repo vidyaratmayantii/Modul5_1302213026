394b640ee6b1d500055f117c284b4639d7c3bf06
ashar <vidyaanandar@gmail.com>
2023-03-24 09:13:30 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using mod5_1302213026;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SimpleDataBase<int> simpleData = new SimpleDataBase<int>();
+        simpleData.AddNewData(12);
+        simpleData.AddNewData(34);
+        simpleData.AddNewData(56);
+    }
+}
---
+++ SimpleDataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod5_1302213026
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storeData;
+        private List<DateTime> inputDates;
+        public SimpleDataBase() {
+           this.storeData = new List<T>();
+            this.inputDates = new List<DateTime>(); 
+        }
+        public void AddNewData(T addnewdata) { 
+            this.storeData.Add(addnewdata);
+            this.inputDates.Add(DateTime.Now);
+
+        }
+
+        public void PrintAllData() { 
+            for(int i = 0; i<this.storeData.Count; i++)
+            {
+                Console.WriteLine("Data "+ i +" berisi: "+ storeData[i]+" , yang disimpan pada waktu "+ inputDates[i]);
+            }
+        }
+    }
+}
